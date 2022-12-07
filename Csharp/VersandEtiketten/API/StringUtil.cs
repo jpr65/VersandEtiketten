26c94b790e59c7b8203a3a6d8b9b4ae100891742
jpr65 <ralf.peine@gmx.de>
2022-12-07 11:21:45 +0100

    API/StringUtil.cs - vergessen zu archivieren

---
+++ Csharp/VersandEtiketten/API/StringUtil.cs
@@ -0,0 +1,54 @@
+﻿namespace API
+{
+    /// <summary>
+    /// API: Utilities für Strings, auch aufrufbar, falls der str == null ist
+    ///
+    ///i/    Diese API-Klasse darf nur die DOTNET-APIs aufrufen
+    ///i/    Deshalb werden APIs als eigene Library im Projekt abgelegt.
+    ///i/    Daten-, Operations- und Integrationsklassen sind für die API nicht sichtbar.
+    /// </summary>
+    public class StringUtil
+    {
+        /// <summary>
+        /// Wandelt in Großbuchstaben um und fängt str == null ab.
+        /// </summary>
+        public static String? ToUpperCase(String? str)
+        {
+            if (str == null)
+            {
+                return str;
+            }
+
+            return str.ToUpperInvariant();
+        }
+
+        /// <summary>
+        /// Anders als bei String.Join() wird der Separator nur angehängt,
+        /// wenn beide Strings nicht (leer oder null) sind und der Separator != null ist.
+        /// </summary>
+        public static String? AddOptionalPart(String? leftPartStr, String? newPartStr, String? separator = " ")
+        {
+
+            if (leftPartStr == null)
+            {
+                return newPartStr;
+            }
+
+            if (newPartStr == null)
+            {
+                return leftPartStr;
+            }
+
+            String resultStr = leftPartStr;
+
+            if (separator != null && !String.IsNullOrWhiteSpace(resultStr) && !String.IsNullOrWhiteSpace(newPartStr))
+            {
+                resultStr += separator;
+            }
+
+            resultStr += newPartStr;
+
+            return resultStr;
+        }
+    }
+}
