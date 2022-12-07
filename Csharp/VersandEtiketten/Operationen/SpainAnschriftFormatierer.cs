19e39929505b8bab132046d3695736985a29a914
jpr65 <ralf.peine@gmx.de>
2022-12-07 11:50:37 +0100

    Operationen/SpainAnschriftFormatierer.cs - neu inkl. Tests

---
+++ Csharp/VersandEtiketten/Operationen/SpainAnschriftFormatierer.cs
@@ -0,0 +1,135 @@
+﻿using API;
+using Daten;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operationen
+{
+    /// <summary>
+    /// Operation: Anschrift formatieren, Variante für ESP (Spanien)
+    /// https://www.upu.int/en/Postal-Solutions/Programmes-Services/Addressing-Solutions
+    /// Siehe Doku-Ordner, espEn.pdf
+    /// </summary>
+    public class SpainAnschriftFormatierer : IAnschriftFormatierer
+    {
+
+        private String laenderKennung = "ESP";
+
+
+        public String GetLaenderKennung()
+        {
+            return laenderKennung;
+        }
+
+
+        public List<String?> AnschriftErstellung(Kunde kundeArg, Adresse adresseArg)
+        {
+            List<String?> anschriftListe = new List<String?>();
+
+            if (kundeArg.PrivatKunde)
+            {
+                anschriftListe = this.KundeAnschriftErstellung(anschriftListe, kundeArg);
+            }
+            else
+            {
+                anschriftListe = this.FirmaAnschriftErstellung(anschriftListe, kundeArg);
+            }
+
+            anschriftListe = this.AdresseErstellung(anschriftListe, adresseArg);
+
+            return anschriftListe;
+        }
+
+        private List<String?> KundeAnschriftErstellung(List<String?> anschriftListe, Kunde kundeArg)
+        {
+            String? zeileNamen = kundeArg.Titel;
+
+            zeileNamen = StringUtil.AddOptionalPart(zeileNamen, kundeArg.Vorname);
+            zeileNamen = StringUtil.AddOptionalPart(zeileNamen, kundeArg.Name);
+
+            anschriftListe.Add(zeileNamen);
+
+            return anschriftListe;
+        }
+
+        private List<String?> FirmaAnschriftErstellung(List<String?> anschriftListe, Kunde kundeArg)
+        {
+            anschriftListe.Add(kundeArg.Firma);
+
+            if (!String.IsNullOrWhiteSpace(kundeArg.Abteilung))
+            {
+                anschriftListe.Add(kundeArg.Abteilung);
+            }
+
+            return anschriftListe;
+        }
+
+        private List<String?> AdresseErstellung(List<String?> anschriftListe, Adresse adresseArg)
+        {
+            this.ErstelleStrasseZeile(anschriftListe, adresseArg);
+            this.ErstelleOrtsteilZeile(anschriftListe, adresseArg);
+            this.ErstelleOrtsZeile(anschriftListe, adresseArg);
+            this.ErstelleProvinzZeile(anschriftListe, adresseArg);
+            this.ErstelleLandZeile(anschriftListe, adresseArg);
+
+            return anschriftListe;
+        }
+
+        private void ErstelleStrasseZeile(List<String?> anschriftListe, Adresse adresseArg)
+        {
+            String? strasseZeile = adresseArg.Strasse;
+
+            strasseZeile = StringUtil.AddOptionalPart(strasseZeile, adresseArg.Hausnummer);
+            strasseZeile = StringUtil.AddOptionalPart(strasseZeile, adresseArg.Flur, ", ");
+            strasseZeile = StringUtil.AddOptionalPart(strasseZeile, adresseArg.Tuer, ", ");
+
+            if (!String.IsNullOrWhiteSpace(strasseZeile))
+            {
+                anschriftListe.Add(strasseZeile);
+            }
+        }
+
+        private void ErstelleOrtsteilZeile(List<String?> anschriftListe, Adresse adresseArg)
+        {
+            if (!String.IsNullOrWhiteSpace(adresseArg.Ortsteil))
+            {
+                anschriftListe.Add(adresseArg.Ortsteil);
+            }
+        }
+
+        private void ErstelleOrtsZeile(List<String?> anschriftListe, Adresse adresseArg)
+        {
+            String? ortZeile = adresseArg.PostLeitInfo;
+
+            ortZeile = StringUtil.AddOptionalPart(ortZeile, StringUtil.ToUpperCase(adresseArg.Ort));
+
+            if (!String.IsNullOrWhiteSpace(ortZeile))
+            {
+                anschriftListe.Add(ortZeile);
+            }
+        }
+
+        private void ErstelleLandZeile(List<String?> anschriftListe, Adresse adresseArg)
+        {
+            String? land = StringUtil.ToUpperCase(adresseArg.Land);
+
+            if (!String.IsNullOrWhiteSpace(land))
+            {
+                anschriftListe.Add(land);
+            }
+        }
+
+        private void ErstelleProvinzZeile(List<String?> anschriftListe, Adresse adresseArg)
+        {
+            String? provinz = StringUtil.ToUpperCase(adresseArg.Provinz);
+
+            if (!String.IsNullOrWhiteSpace(provinz))
+            {
+                anschriftListe.Add(provinz);
+            }
+        }
+    }
+}
---
+++ Csharp/VersandEtiketten/TestOperationen/TCL_0120_SpainAnschriftFormatierer.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOperationen
+{
+    [TestClass]
+    public class TCL_0120_SpainAnschriftFormatierer
+    {
+        #region Testvorbereitung ----------------------------------------------------
+        private Adresse CreateAdresseLeer()
+        {
+            return new Adresse();
+        }
+
+        private Kunde CreateKundeLeer()
+        {
+            return new Kunde();
+        }
+
+        private Adresse CreateAdresse1()
+        {
+            Adresse adresse = new Adresse();
+
+            adresse.Strasse = "Calle Huertas";
+            adresse.Hausnummer = "18";
+            adresse.Flur = "4°";
+            adresse.Tuer = "C";
+            adresse.PostLeitInfo = "09692";
+            adresse.Ort = "Canicosa De La Sierra";
+            adresse.Ortsteil = "Barrio de San Roque";
+            adresse.Provinz = "Burgos";
+            adresse.Land = "Spain";
+
+            return adresse;
+        }
+
+        private Adresse CreateAdresse2()
+        {
+            Adresse adresse = new Adresse();
+
+            adresse.PostLeitInfo = "08028";
+            adresse.Ort = "Barcelona";
+            adresse.Strasse = "Av. Joan XXIII";
+            adresse.Flur = "s/n";
+            adresse.Land = "Spain";
+
+            return adresse;
+        }
+
+        private Kunde CreateKunde1()
+        {
+            Kunde kunde = new Kunde();
+
+            kunde.Vorname = "Naviera";
+            kunde.Name = "Casarejos";
+            kunde.PrivatKunde = true;
+
+            return kunde;
+        }
+
+        private Kunde CreateFirmenKunde()
+        {
+            Kunde firmenKunde = new Kunde();
+
+            firmenKunde.PrivatKunde = false;
+            firmenKunde.Firma = "FC Barcelona";
+            firmenKunde.Abteilung = "Shop";
+
+            return firmenKunde;
+        }
+
+        private Kunde CreateFirmenKundeOhneAbteilung()
+        {
+            Kunde firmenKunde = new Kunde();
+
+            firmenKunde.PrivatKunde = false;
+            firmenKunde.Firma = "International Imports";
+
+            return firmenKunde;
+        }
+        #endregion
+
+
+        /**
+         * Test method for {@link Operationen.SpainAnschriftFormatierer#GetLaenderkennung()}.
+         */
+        [TestMethod]
+        public void T001_GetLaenderkennung()
+        {
+
+            // --- Testvorbereitung --------------------------------
+
+            SpainAnschriftFormatierer testObject = new SpainAnschriftFormatierer();
+
+            // --- Testdurchführung --------------------------------
+
+            String laenderKennungResult = testObject.GetLaenderKennung();
+
+            // --- Testvalidierung --------------------------------
+
+            Assert.AreEqual("esp", laenderKennungResult.ToLowerInvariant());
+        }
+
+        /**
+         * Test method for {@link Operationen.SpainAnschriftFormatierer#AnschriftErstellung(Kunde kundeArg, Adresse adresseArg)}.
+         */
+        [TestMethod]
+        public void T010_AnschriftErstellung_leer()
+        {
+
+            // --- Testvorbereitung --------------------------------
+
+            Kunde kunde = this.CreateKundeLeer();
+            Adresse adresse = this.CreateAdresseLeer();
+
+            SpainAnschriftFormatierer testObject = new SpainAnschriftFormatierer();
+
+            // --- Testdurchführung --------------------------------
+
+            List<String?> testResult = testObject.AnschriftErstellung(kunde, adresse);
+
+            // --- Testvalidierung --------------------------------
+
+            Assert.AreEqual(1, testResult.Count);
+            Assert.AreEqual("", testResult[0]);
+        }
+
+        /**
+         * Test method for {@link Operationen.SpainAnschriftFormatierer#AnschriftErstellung(Kunde kundeArg, Adresse adresseArg)}.
+         */
+        [TestMethod]
+        public void T100_AnschriftErstellung_PrivatKunde()
+        {
+
+            // --- Testvorbereitung --------------------------------
+
+            Kunde kunde = this.CreateKunde1();
+            Adresse adresse = this.CreateAdresse1();
+
+            SpainAnschriftFormatierer testObject = new SpainAnschriftFormatierer();
+
+            // --- Testdurchführung --------------------------------
+
+            List<String?> testResult = testObject.AnschriftErstellung(kunde, adresse);
+
+            // --- Testvalidierung --------------------------------
+
+            Assert.AreEqual(6, testResult.Count);
+            int lineNbr = 0;
+            Assert.AreEqual("Naviera Casarejos", testResult[lineNbr++]);
+            Assert.AreEqual("Calle Huertas 18, 4°, C", testResult[lineNbr++]);
+            Assert.AreEqual("Barrio de San Roque", testResult[lineNbr++]);
+            Assert.AreEqual("09692 CANICOSA DE LA SIERRA", testResult[lineNbr++]);
+            Assert.AreEqual("BURGOS", testResult[lineNbr++]);
+            Assert.AreEqual("SPAIN", testResult[lineNbr++]);
+        }
+
+        /**
+         * Test method for {@link Operationen.SpainAnschriftFormatierer#AnschriftErstellung(Kunde kundeArg, Adresse adresseArg)}.
+         */
+        [TestMethod]
+        public void T110_AnschriftErstellung_FirmenKunde()
+        {
+
+            // --- Testvorbereitung --------------------------------
+
+            Kunde kunde = this.CreateFirmenKunde();
+            Adresse adresse = this.CreateAdresse2();
+
+            SpainAnschriftFormatierer testObject = new SpainAnschriftFormatierer();
+
+            // --- Testdurchführung --------------------------------
+
+            List<String?> testResult = testObject.AnschriftErstellung(kunde, adresse);
+
+            // --- Testvalidierung --------------------------------
+
+            Assert.AreEqual(5, testResult.Count);
+            int lineNbr = 0;
+            Assert.AreEqual("FC Barcelona", testResult[lineNbr++]);
+            Assert.AreEqual("Shop", testResult[lineNbr++]);
+            Assert.AreEqual("Av. Joan XXIII, s/n", testResult[lineNbr++]);
+            Assert.AreEqual("08028 BARCELONA", testResult[lineNbr++]);
+            Assert.AreEqual("SPAIN", testResult[lineNbr++]);
+        }
+
+        /**
+         * Test method for {@link Operationen.SpainAnschriftFormatierer#AnschriftErstellung(Kunde kundeArg, Adresse adresseArg)}.
+         */
+        [TestMethod]
+        public void T120_AnschriftErstellung_FirmenKundeOhneAbteilung()
+        {
+
+            // --- Testvorbereitung --------------------------------
+
+            Kunde kunde = this.CreateFirmenKundeOhneAbteilung();
+            Adresse adresse = this.CreateAdresse2();
+
+            SpainAnschriftFormatierer testObject = new SpainAnschriftFormatierer();
+
+            // --- Testdurchführung --------------------------------
+
+            List<String?> testResult = testObject.AnschriftErstellung(kunde, adresse);
+
+            // --- Testvalidierung --------------------------------
+
+            Assert.AreEqual(4, testResult.Count);
+            int lineNbr = 0;
+            Assert.AreEqual("International Imports", testResult[lineNbr++]);
+            Assert.AreEqual("Av. Joan XXIII, s/n", testResult[lineNbr++]);
+            Assert.AreEqual("08028 BARCELONA", testResult[lineNbr++]);
+            Assert.AreEqual("SPAIN", testResult[lineNbr++]);
+        }
+    }
+}
